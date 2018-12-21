2a3c562e7de9c28d92b759b9b185e62bd768ecfd
caroline burns <carolineburns@carolines-MacBook.local>
2018-12-21 13:58:48 -0700

    worked on adding the addItem list

--- src/App.js
+++ src/App.js
@@ -3,22 +3,39 @@ import './App.css';
 import Footer from './components/footer.js';
 import Header from './components/header.js';
 import CartItems from './components/cartItems.js'; 
+import AddItem from './components/addItem.js'
 
 
 class App extends Component {
   constructor() {
     super() 
     this.state = {
-      shopping: ''
+      products: [
+        { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
+        { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
+        { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
+        { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
+        { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
+        { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
+        { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
+        { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
+        { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
+      ],
+      cartList: []
     }
   }
   
+  addItem = (event) => {
+    event.preventDefault()
+  }
+    //todo add item to cart list
+
   render() {
     const cartItemsList =   
       [
-        { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
-        { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
-        { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 }, quantity: 1 },
+        { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 3.99 }, quantity: 1 },
+        { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 4.99 }, quantity: 2 },
+        { id: 3, product: { id: 42, name: 'Intelligent Paper Knife', priceInCents: 19.99 }, quantity: 1 },
       ]
 
     return (
@@ -26,8 +43,11 @@ class App extends Component {
         <Header/>
         <CartItems
           cartItemsList = {cartItemsList}/>
+        <AddItem addItem={this.addItem}/> 
+        <AddItem products={this.state.products} addItem={this.addItem}/>
         <Footer
           copyright={ 2016}/> 
+         
       </div>
     )
       
---
+++ src/components/addItem.js
@@ -0,0 +1,42 @@
+import React from 'react'; 
+
+const AddItem = (props) => {
+    console.log(props)
+
+    let newItem = {
+        product: {
+          id: 40,
+          name: 'Mediocre Iron Watch',
+          priceInCents: 399
+        },
+        quantity: 1
+      }
+
+    return (
+        <div className="container">
+        <form onSubmit={props.addItem}>  
+        <div class="form-group">
+            <label for="exampleInputEmail1">Quantity</label>
+            <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Quantity"/>
+            
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlSelect1">Example select</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+            <option>Apple</option>
+            <option>Banana</option>
+            <option>Potato</option>
+            {props.products.map(product => <option key={product.id} value={product.id}>{product.name}</option>)}
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    );
+
+}
+
+export default AddItem
+
+
+
